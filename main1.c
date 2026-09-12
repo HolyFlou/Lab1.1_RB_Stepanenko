64b4b18603052765a6c182e916650a134b5ebc00
HolyFlou <stepanenko.nekita2020@gmail.com>
2026-09-12 13:31:22 +0300

    Виправив коментарі

--- main1.c
+++ main1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//---------Мікровизначення---------
+//---------Макровизначення---------
 #define Name       "Nikita"
 #define LastName   "Stepanenko"
 #define GroupName  "RB-61"
